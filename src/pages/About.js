6be9d73154c063451f22779cbbdef68171c4acb4
Ahmet Ustun <austu@softserveinc.com>
2023-03-27 10:16:53 +0300

    Create a logic to imitate logout in order to test the Navigate component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 function About() {
+  const [user, setUser] = useState("mario");
+
+  if (!user) {
+    return <Navigate to="/" replace={true} />;
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
@@ -20,6 +29,7 @@ function About() {
         eveniet iusto? Eius nostrum quos aspernatur nesciunt fugiat perferendis?
         Ab, minus!
       </p>
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 }
